953b162b6a412d468f9fef3f9e7388b34c0434a7
Thomas Salmon <th_s@hotmail.fr>
2014-10-04 20:18:38 +0200

    arguments
    arguments -> variables

--- F5Song.cs
+++ F5Song.cs
@@ -9,29 +9,115 @@ namespace F5Song
 {
     class F5SongClass
     {
-        static List<TagLib.File> songs;
+        private static List<TagLib.File>    songs;
+        private static bool                 arg_text;
+        private static string               arg_begin;
+        private static string               arg_end;
+        private static int                  arg_incTrack;
+        private static string[]             arg_involvedartist;
+        private static string               arg_albumartist;
+        private static string               arg_year;
+        private static string[]             arg_genre;
+        private static string               arg_album_name;
+        private static string               arg_album_path;
 
         public static void help()
-        {
-            Console.WriteLine("<album>");
-            Console.WriteLine("[-begin=X] [-h] [-end=Y] [-track=A] [-text=c|l] [-space=u|s] [-ascii]");
+        {               
+            Console.WriteLine("F5Song - un programme pour editer les mp3");
+            
+            Console.WriteLine("Argument obligatoire");
+            Console.WriteLine("<path album>");
+            
+            Console.WriteLine("Arguments de selection"); 
+            Console.WriteLine("[-begin=X] debut des numeros de mp3 selectionées");
+            Console.WriteLine("[-end=Y] fin des numeros de mp3 selectionnées");
+
+            Console.WriteLine("Arguments optionels");
+            Console.WriteLine("[-track=A] [-text] [-involved=] [-albumartist=] [-year=] [-genre=]");
         }
 
-        private static void arguments(string[] args)
+        private static string[] stringToArray(string str, char sep)
         {
-            
+            string[] words = str.Split(sep);
+            return words;
+        }
+
+        private static bool setArguments(string arg_var){
+            string[] words = arg_var.Split('=');
+            if(words.Length != 2){
+                Console.WriteLine("Argument invalide : " + arg_var);
+                return false;
+            }
+            switch(words[0])
+            {
+                case "-b":
+                case "-begin":
+                    arg_begin = words[0];
+                    break;
+                case "-e":
+                case "-end":
+                    arg_end = words[1];
+                    break;
+                case "-t":
+                case "-track":
+                    arg_incTrack = Convert.ToInt32(words[1]);
+                    break;
+                case "-txt":
+                case "-text":
+                    arg_text = true;
+                    break;
+                case "-a":
+                case "-albumartist":
+                    arg_albumartist = words[1];
+                    break;
+                case "-n":
+                case "-albumname":
+                    arg_album_name = words[1];
+                    break;
+                case "-i":
+                case "-involved":
+                    arg_involvedartist = stringToArray(words[1], ';');
+                    break;
+                case "-y":
+                case "-year":
+                    arg_year = words[1];          
+                    break;
+                case "-g":
+                case "-genre":
+                    arg_genre = stringToArray(words[1], ';');
+                    break;
+                default:
+                    Console.WriteLine("Argument inexistante: " + arg_var);
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool parseArguments(string[] args)
+        {
+            arg_album_path = args[0];
+            for (int i = 1; i<args.Length; i++)
+            {
+                if(!setArguments(args[i]))
+                {
+                    Console.WriteLine("Cet argument n'existe pas");
+                    return false;
+                }
+            }
+            return true;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(composeString(args, ", "));
 
             if (args.Length < 1)
             {
                 help();
             }
-            else if(args.Length == 999)
+            else
             {
+                parseArguments(args);
+
                 DirectoryInfo album = null;
                 songs = new List<TagLib.File>();
                 
